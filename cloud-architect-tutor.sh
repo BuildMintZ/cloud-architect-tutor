458354b6a8d4edb56e8cdd2a5dc4f3fe4682862c
Cyprain Chidozie <cyprainchidozie232@gmail.com>
2026-05-15 10:52:44 +0200

    Fix formatting in cloud-architect-tutor.sh

--- cloud-architect-tutor.sh
+++ cloud-architect-tutor.sh
@@ -1315,7 +1315,8 @@ resource "aws_instance" "this" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids  key_name               = var.key_name
+  vpc_security_group_ids = var.security_group_ids
+  key_name               = var.key_name
   iam_instance_profile   = var.iam_instance_profile_name != "" ? var.iam_instance_profile_name : null
   associate_public_ip_address = var.associate_public_ip
   root_block_device {
